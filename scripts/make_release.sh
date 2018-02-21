86606b2b8954faca2dd0f1905f5c1d63040a3b5a
joeduffy <joe@pulumi.com>
2018-02-20 18:39:10 -0800

    Adopt plugins for @pulumi/github

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -41,7 +41,7 @@ copy_package() {
 }
 
 # Build binaries
-run_go_build "${ROOT}/cmd/pulumi-provider-github"
+run_go_build "${ROOT}/cmd/pulumi-resource-github"
 
 # Copy Packages
 copy_package "${ROOT}/pack/bin/." "@pulumi/github"
