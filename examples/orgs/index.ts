4032f1a6afd342ee3cca842a54781c73f096d4b7
stack72 <public@paulstack.co.uk>
2019-06-28 13:43:21 +0300

    Cleaning up an old include that was completely broken

--- examples/orgs/index.ts
+++ examples/orgs/index.ts
@@ -1,7 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as github from "@pulumi/github";
-import {WebhookConfiguration} from "../../sdk/nodejs";
 
 const name = "orgs";
 
