678287d6a3808cbcf7e96bf93a3d359074999f25
stack72 <public@paulstack.co.uk>
2020-12-08 19:52:21 +0000

    Upgrade integration tests for repository to set visibility rather than private

--- examples/repo/python/__main__.py
+++ examples/repo/python/__main__.py
@@ -3,7 +3,7 @@ import pulumi_github as github
 
 repo = github.Repository("demo-repo",
     description="Generated from automated test",
-    private="true"
+    visibility="private"
 )
 
 pulumi.export('repo_name', repo.name)
--- examples/repo/ts/index.ts
+++ examples/repo/ts/index.ts
@@ -2,7 +2,7 @@ import * as github from "@pulumi/github";
 
 const repo = new github.Repository("demo-repo", {
     description: "Generated from automated test",
-    private: true,
+    visibility: "private",
 });
 
 export const repoName = repo.name;
