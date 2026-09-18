9f15a666e6d724c06365504333d990c13faaa4cb
Cory Hall <43035978+corymhall@users.noreply.github.com>
2026-09-18 12:13:19 +0000

    Remove deprecated bridge flags flagged by staticcheck lint
    EnableAccurateBridgePreview and EnableRawStateDelta are deprecated in
    pulumi-terraform-bridge v3.139.0 (enabled by default now) and their use
    was failing the lint / lint CI job with SA1019 staticcheck errors.

--- provider/resources.go
+++ provider/resources.go
@@ -264,8 +264,6 @@ func Provider() tfbridge.ProviderInfo {
 			},
 		},
 		EnableZeroDefaultSchemaVersion: true,
-		EnableAccurateBridgePreview:    true,
-		EnableRawStateDelta:            true,
 	}
 
 	prov.MustComputeTokens(tfbridgetokens.SingleModule("github_", mainMod,
